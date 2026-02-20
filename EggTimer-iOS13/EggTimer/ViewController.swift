76a6a32dd580d45cd6f9f882016e727de5447dea
Isra de Leon <isra.frontend@gmail.com>
2026-02-20 12:21:05 -0600

    implement progress view & timer indicator

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var countdownLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
     
     let boiledEggTimes: [ String : Int ] = [ "Soft": 5*60, "Medium": 7*60, "Hard": 12*60 ]
     var timer: Timer?
@@ -20,15 +21,30 @@ class ViewController: UIViewController {
         timer?.invalidate()
         
         let hardness = sender.currentTitle!
-        var eggCountdown = boiledEggTimes[hardness]!
+        let cookingTime = boiledEggTimes[hardness]!
+        var eggCountdown = cookingTime
         
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
             self.countdownLabel.text = "\(eggCountdown) seconds left..."
+            self.progressView.progress = self.calculateProgress(
+                totalTime: cookingTime,
+                countDown: eggCountdown
+            )
+        
             eggCountdown -= 1
             if eggCountdown == 0 {
                 timer.invalidate()
                 self.countdownLabel.text = "Done!"
             }
         }
+    }
+    
+    func calculateProgress(
+        totalTime: Int,
+        countDown: Int
+    ) -> Float {
+        let elapsedTime = totalTime - countDown
+        let percentage = Float(elapsedTime * 100 / totalTime)
+        return percentage * 0.01
     }
 }
