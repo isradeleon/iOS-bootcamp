a2fdcf988a089f536bf486874a14fadef7823467
Isra de Leon <isra.frontend@gmail.com>
2026-02-23 11:22:41 -0600

    impl multi dimensional array

--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
+    
+    var currentQuestion = 0
+    let quiz = [
+        [ "Four + Two is equal to Six.", "True" ],
+        [ "Five - Three is greater than One.", "False" ],
+        [ "Three + Eight is less than Ten.", "False" ]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        refreshQuestionUI()
+    }
+    
+    @IBAction func answerPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let correctAnswer = quiz[currentQuestion][1]
+        
+        if userAnswer == correctAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        if currentQuestion < quiz.count-1 {
+            currentQuestion += 1
+            refreshQuestionUI()
+        }
     }
 
-
+    func refreshQuestionUI() {
+        questionLabel.text = quiz[currentQuestion][0]
+    }
 }
 
