f47f7afb447a64905c1513f59aef360580281619
Isra de Leon <isra.frontend@gmail.com>
2026-02-25 08:52:29 -0600

    impl Story struct

--- Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let stories = [
+        Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a left"),
+        Story(title: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
+        Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps"),
+    ]
+    
+    var userPosition = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        refreshUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if sender.currentTitle == stories[userPosition].choice1 {
+            userPosition = 1
+        } else {
+            userPosition = 2
+        }
+        
+        refreshUI()
+    }
+    
+    func refreshUI() {
+        storyLabel.text = stories[userPosition].title
+        choice1Button.setTitle(stories[userPosition].choice1, for: .normal)
+        choice2Button.setTitle(stories[userPosition].choice2, for: .normal)
+    }
+    
 }
 
