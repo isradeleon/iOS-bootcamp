45215ead63157aa3940128e1066736a9935214d4
Isra de Leon <isra.frontend@gmail.com>
2026-02-26 15:54:00 -0600

    impl BMI struct to better handle bmi value cases

---
+++ BMI-Calculator-iOS13/BMI Calculator/Model/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Israel Diaz de Leon on 26/02/26.
+//  Copyright © 2026 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
--- BMI-Calculator-iOS13/BMI Calculator/Model/BodyMassIndexCalculator.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Model/BodyMassIndexCalculator.swift
@@ -9,15 +9,24 @@
 import Foundation
 
 struct BodyMassIndexCalculator {
-    private var bmi: Float?
+    private var bmi: BMI?
     
     mutating func calculate(
         height: Float, weight: Float
     ) {
-        bmi = weight / pow(height, 2)
+        let result = weight / pow(height, 2)
+        bmi = BMI(
+            value: result, advice: "", color: .systemGreen
+        )
+        
+        switch result {
+        case ..<18.5: print("Underweight")
+        case 18.5...24.9: print("Normal")
+        default: print("Overweight")
+        }
     }
     
     func getBMIResult() -> String {
-        return String(format: "%.1f", bmi ?? 0)
+        return String(format: "%.1f", bmi?.value ?? 0)
     }
 }
