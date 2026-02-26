4f127773c86a64c7e515828de10fd0cbf6126442
Isra de Leon <isra.frontend@gmail.com>
2026-02-26 11:57:43 -0600

    create resultsVC IBOutlets & rename original ViewController

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultsViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultsViewController.swift
@@ -10,13 +10,19 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+    }
+    
     /*
     // MARK: - Navigation
 
