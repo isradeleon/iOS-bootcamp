fb6b3191a47a3a4ea76a641e23b774aac08e0b17
Isra de Leon <isra.frontend@gmail.com>
2026-02-26 11:09:22 -0600

    create custom view controller class

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/BMIViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BMIViewController.swift
+//  BMI Calculator
+//
+//  Created by Israel Diaz de Leon on 26/02/26.
+//  Copyright © 2026 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class BMIViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
