cbc5657b6aed2cd90b1f09a82c605ff5c5842a4f
Isra de Leon <isra.frontend@gmail.com>
2026-02-26 12:41:17 -0600

    add dismiss call to recalculate BMI

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultsViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ResultsViewController.swift
@@ -29,7 +29,7 @@ class ResultsViewController: UIViewController {
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
-        
+        self.dismiss(animated: true)
     }
     
     /*
