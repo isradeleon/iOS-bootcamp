8944405fed23f027f2d93c711d5238f35ad267f2
Isra de Leon <isra.frontend@gmail.com>
2026-03-03 12:47:05 -0600

    impl URL, URLSession & DataTask to fetch weather data

--- Clima-iOS13/Clima/Model/OpenWeatherManager.swift
+++ Clima-iOS13/Clima/Model/OpenWeatherManager.swift
@@ -6,11 +6,40 @@
 //  Copyright © 2026 App Brewery. All rights reserved.
 //
 
+import Foundation
+
 struct OpenWeatherManager {
     private let baseUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric"
     private let apiKey = "cdbf5d9374361aa56ac4c63e1e82d0d8"
     
     func fetchWeather(city: String) {
         let requestUrl = "\(baseUrl)&appid=\(apiKey)&q=\(city)"
+        performRequest(urlString: requestUrl)
+    }
+    
+    private func performRequest(urlString: String) {
+        // 1. Create a URL object
+        if let url = URL(string: urlString) {
+            // 2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give the sessiona task
+            let task = session.dataTask(with: url, completionHandler: handler(data: response: error:))
+            
+            // 4. Start task (call resume method since they're suspended by default)
+            task.resume()
+        }
+    }
+    
+    private func handler(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!)
+        }
     }
 }
