19b967f6ba30972d178007d8866987e77711c1e6
Isra de Leon <isra.frontend@gmail.com>
2026-03-03 11:03:55 -0600

    add extra comment about protocol impl inheritance

--- Delegate Design Pattern/Delegate Design Pattern/objects/Surgeon.swift
+++ Delegate Design Pattern/Delegate Design Pattern/objects/Surgeon.swift
@@ -5,6 +5,7 @@
 //  Created by Israel Diaz de Leon on 03/03/26.
 //
 
+// Through inheritance, the Surgeon class inherits the Doctor protocol impl
 class Surgeon: Doctor {
     override func performCPR() {
         print("Performs CPR & uses Defibrillator on the patient.")
