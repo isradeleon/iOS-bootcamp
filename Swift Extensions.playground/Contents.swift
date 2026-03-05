40ebe278dbb69280789dd70af411547e5c7a5fc4
Isra de Leon <isra.frontend@gmail.com>
2026-03-05 10:29:16 -0600

    add multiple use cases for Swift extensions (primitive types, objects & protocols)

--- Swift Extensions.playground/Contents.swift
+++ Swift Extensions.playground/Contents.swift
@@ -1,3 +1,64 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/**
+ Extending an open source Swift native type class
+ */
+extension Double {
+    func rounded(to decimalPlaces: Int) -> Double {
+        let precisionNum = pow(10, Double(decimalPlaces))
+        var auxNum = self
+        auxNum *= precisionNum
+        auxNum.round()
+        auxNum /= precisionNum
+        return auxNum
+    }
+}
+
+let aNumber = 3.14159
+print(aNumber.rounded(to: 2))
+
+/**
+ Extending a UIKit class
+ */
+extension UIButton {
+    func adjustRadius(to corners: Int) {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = CGFloat(corners)
+    }
+    
+    func adjustToCircle() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = self.frame.size.width / 2
+    }
+}
+
+let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+button.backgroundColor = .systemRed
+
+button.adjustRadius(to: 10)
+button.adjustToCircle()
+
+/**
+ Defining default protocol implementations through extensions
+ */
+protocol NetworkRequestsDelegate {
+    func onRequestSuccess()
+    func onRequestFailure(error: Error)
+    func onTimeout()
+}
+
+extension NetworkRequestsDelegate {
+    func onRequestFailure(error: Error) {
+        print(error)
+    }
+    
+    func onTimeout() {
+        print("Timeout exception during network request...")
+    }
+}
+
+class DummyClass: NetworkRequestsDelegate {
+    func onRequestSuccess() {
+        print("Now you're only forced to implement the success method")
+    }
+}
