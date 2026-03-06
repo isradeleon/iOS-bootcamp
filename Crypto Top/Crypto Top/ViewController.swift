4bc26216edf77f00946eaab827a5e79c415fac73
Isra de Leon <isra.frontend@gmail.com>
2026-03-06 10:38:29 -0600

    create first view controller screen

--- Crypto Top/Crypto Top/ViewController.swift
+++ Crypto Top/Crypto Top/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstCryptoImg: UIImageView!
+    @IBOutlet weak var secondCryptoImg: UIImageView!
+    @IBOutlet weak var thirdCryptoImg: UIImageView!
+    
+    @IBOutlet weak var firstCyptoLabel: UILabel!
+    @IBOutlet weak var secondCryptoLabel: UILabel!
+    @IBOutlet weak var thirdCryptoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func cryptoDetailsPressed(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
