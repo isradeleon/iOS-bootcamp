d8f4b6150fed3dd288eea5196b39c4a540e2847f
Isra de Leon <isra.frontend@gmail.com>
2026-03-12 15:37:42 -0600

    print multiple for loops with many types of collections

--- Swift Loops.playground/Contents.swift
+++ Swift Loops.playground/Contents.swift
@@ -1,12 +1,35 @@
 import Foundation
 
+// Arrays
 let fruits = ["Apple", "Pear", "Orange"]
+for fruit in fruits { print(fruit) }
+
+print("---------------")
 
 // Sets don't guarantee the order, but are considered more efficient:
 let fruitsSet: Set = ["Apple", "Pear", "Orange"]
+for fruit in fruitsSet { print(fruit) }
 
+print("---------------")
+
+// Looping through a map
 let contacts = ["Adam": 1232434332, "Amy": 6787875767]
-let text = "There was a time..."
-let halfOpenRange = 1..<5
-let closedRange = 1...5
+for contact in contacts { print("\(contact.key) - \(contact.value)") }
+for (contact, number) in contacts { print("\(contact) - \(number)") }
 
+print("---------------")
+
+// Looping through text
+let text = "There was a time..."
+for letter in text { print(letter) }
+
+print("---------------")
+
+// Looping through a range
+let halfOpenRange = 1..<5
+for n in halfOpenRange { print(n) }
+
+print("---------------")
+
+// For loop without anonymous constant
+for _ in 1...5 { print("I'll be printed 5 times!") }
