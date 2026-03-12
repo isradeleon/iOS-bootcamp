0edb97d0e59bf29bf0f95233b6d827021a48ebe2
Isra de Leon <isra.frontend@gmail.com>
2026-03-12 15:28:49 -0600

    create swift loops playground

---
+++ Swift Loops.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let fruits = ["Apple", "Pear", "Orange"]
+
+// Sets don't guarantee the order, but are considered more efficient:
+let fruitsSet: Set = ["Apple", "Pear", "Orange"]
+
+let contacts = ["Adam": 1232434332, "Amy": 6787875767]
+let text = "There was a time..."
+let halfOpenRange = 1..<5
+let closedRange = 1...5
+
