ba8d1da0066694b6c7144fecbc8bdb90281f85b4
Isra de Leon <isra.frontend@gmail.com>
2026-03-13 11:38:03 -0600

    implement CLTypingLabel pod dependency & fix conflicting deployment targets

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,16 +7,18 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        animateTitleLabel(title: "⚡️FlashChat")
+        titleLabel.text = "⚡️FlashChat"
     }
     
+    // Manually animating the UILabel
     func animateTitleLabel(title: String) {
         titleLabel.text = ""
         var millisOffset = 0.1
