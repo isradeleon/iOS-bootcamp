0dae6e3b4d1fcad2d6820a8fa5552741440233df
Isra de Leon <isra.frontend@gmail.com>
2026-03-14 15:33:28 -0600

    implement email & password validation in registration screen

--- Firestore, TableViews & Cocoapods/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Firestore, TableViews & Cocoapods/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +15,25 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        guard let email = emailTextfield.text, let password = passwordTextfield.text
+        else { return }
+        
+        guard isValidEmail(email) && isValidPassword(password)
+        else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            
+        }
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegex = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+    
+    func isValidPassword(_ password: String) -> Bool {
+        return !password.isEmpty && password.count >= 5
     }
     
 }
